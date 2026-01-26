e6eb232f85946e234e36102c15a4520c4fbf0de1
aleynakkan <70470415+aleynakkan@users.noreply.github.com>
2026-01-26 04:37:48 -0800

    footer revize

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,44 +10,44 @@ const imgVector6 = "https://www.figma.com/api/mcp/asset/37455917-a553-4124-80c8-
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <div className={styles.footerCol}>
+      <div className={styles.grid}>
+        <div className={styles.column}>
           <h3>Seyfo</h3>
-          <ul>
+          <ul className={styles.links}>
             <li><a href="#about">Hakkımızda</a></li>
             <li><a href="#features">Özellikler</a></li>
             <li><a href="#help">Yardım Merkezi</a></li>
           </ul>
         </div>
-        <div className={styles.footerCol}>
-          <h3>Çözümler</h3>
-          <ul>
+        <div className={styles.column}>
+          <h3>3c3cmler</h3>
+          <ul className={styles.links}>
             <li><a href="#sme">KOBİ İçin Finans</a></li>
             <li><a href="#ecommerce">E-ticaret İçin Çözümler</a></li>
             <li><a href="#donation">Bağış Kampanyaları</a></li>
             <li><a href="#multi">Çok Ortaklı İşletmeler</a></li>
           </ul>
         </div>
-        <div className={styles.footerCol}>
+        <div className={styles.column}>
           <h3>Destek</h3>
-          <ul>
+          <ul className={styles.links}>
             <li><a href="#faq">Sıkça Sorulan Sorular</a></li>
             <li><a href="#contact">İletişim</a></li>
             <li><a href="#privacy">Gizlilik</a></li>
           </ul>
         </div>
-        <div className={styles.footerCol}>
+        <div className={styles.column}>
           <h3>Takip Edin</h3>
-          <div className={styles.footerSocials}>
-            <a href="#"><img src={imgVector} alt="" /></a>
-            <a href="#"><img src={imgVector1} alt="" /></a>
-            <a href="#"><img src={imgVector4} alt="" /></a>
-            <a href="#"><img src={imgVector6} alt="" /></a>
+          <div className={styles.socialLinks}>
+            <a href="#" className={styles.socialIcon}><img src={imgVector} alt="" /></a>
+            <a href="#" className={styles.socialIcon}><img src={imgVector1} alt="" /></a>
+            <a href="#" className={styles.socialIcon}><img src={imgVector4} alt="" /></a>
+            <a href="#" className={styles.socialIcon}><img src={imgVector6} alt="" /></a>
           </div>
         </div>
       </div>
-      <div className={styles.footerBottom}>
-        <span>© 2026 Seyfo. Tüm hakları saklıdır.</span>
+      <div className={styles.bottom}>
+          <span>© 2026 Seyfo. Tüm hakları saklıdır.</span>
       </div>
     </footer>
   );
